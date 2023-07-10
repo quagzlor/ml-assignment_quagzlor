d3302611536fd6f77f8912be1a4a6261cb607ce1
quagzlor <divijsingh@gmail.com>
2023-07-11 01:11:37 +0900

    Got the basic code working

--- app/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.post("/translation")
-def translation(src_text: str, target_lang: str):
-    return {"result": "The target."}
--- app/main.py
+++ app/main.py
@@ -1,10 +1,30 @@
 from fastapi import FastAPI
 
+from data_schemas import Request
+from M2M100_model import M2M100Translator
+
 app = FastAPI()
 
+#Health Check
+@app.get("/boop")
+async def health():
+    return {"status": "poob"}
 
-
-
+#To handle the POST request
 @app.post("/translation")
-def translation(text: str,source_language: str,  target_language: str):
-    return {"result": "人生はチョコレートの箱のようなものだ。"}
+def translation(request: Request):
+    payload = request.payload
+
+    translator = M2M100Translator(payload.fromLang, payload.toLang)
+    records = payload.records
+    print(type(records))
+    results = []
+    for entry in payload.records:
+        translated_text = translator.translate(entry.text)
+
+        results.append({
+            "id":entry.id,
+            "text":translated_text
+        })
+
+    return {"result":results}
